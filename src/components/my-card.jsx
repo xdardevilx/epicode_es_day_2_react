6b2e5bac4c91869758fd8539d7b075514b7c2a81
xdardevilx <valeriogalletti21@gmail.com>
2024-01-30 17:00:14 +0100

    fixed img

--- src/components/my-card.jsx
+++ src/components/my-card.jsx
@@ -5,9 +5,9 @@ function MyCard (props) {
 let book=props.book
     return (
 <Card className='flex-fill'>
-    <Card.Img className='img-fit' src={book.img} height={500}/>
-    <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
+    <Card.Img className='img-fit' src={book.img} height={300}/>
+    <Card.Body className='d-flex flex-column'>
+        <Card.Title className='flex-grow-1'>{book.title}</Card.Title>
         <Card.Text>{book.price}</Card.Text>
         <Card.Text>{book.category}</Card.Text>
     </Card.Body>
